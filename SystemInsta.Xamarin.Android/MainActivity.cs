6d0a8b65dcb00ca4073e8105c752716a7dbdc774
Bruno Garcia <bruno@brunogarcia.com>
2019-09-29 21:25:37 +0200

    print debug id

--- SystemInsta.Xamarin.Android/MainActivity.cs
+++ SystemInsta.Xamarin.Android/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Android.App;
@@ -9,6 +10,9 @@ using Android.Widget;
 using static Android.Provider.Settings;
 using Android.Content.PM;
 using Android.Provider;
+using ELFSharp.ELF;
+using ELFSharp.ELF.Sections;
+using StringBuilder = Java.Lang.StringBuilder;
 
 namespace SystemInsta.Xamarin.Android
 {
@@ -57,57 +61,82 @@ App Version: {versionName}'
 App Build: {versionCode}'");
 
                 var paths = new[] {"/system/lib", "/system/lib64", "/system/"};
-                Parallel.ForEach(paths, path =>
-                {
-                    if (Directory.Exists(path))
-                    {
-                        var fileCount = Directory.GetFiles(path).Length;
-                        Log.Info(Tag, $"Path {path} has {fileCount} files to process");
-
-                        var files = Directory.EnumerateFiles(path);
-                        Log.Info(Tag, $"Enumerating {path}.");
-                        foreach (var file in files)
-                        {
-                            Log.Info(Tag, $"File: {file}.");
-                            if (ELFSharp.ELF.ELFReader.TryLoad(file, out var elf))
-                            {
-                                var hasUnwindingInfo = elf.TryGetSection(".eh_frame", out _);
-                                Log.Info(Tag, $"Contains unwinding info: {hasUnwindingInfo}");
-                                var hasDwarfDebugInfo = elf.TryGetSection(".debug_frame", out _);
-                                Log.Info(Tag, $"Contains DWARF debug info: {hasDwarfDebugInfo}");
-
-                                if (hasUnwindingInfo || hasDwarfDebugInfo)
-                                {
-                                    Log.Info(Tag, $"Class: {elf.Class}.");
-                                    Log.Info(Tag, $"HasSectionsStringTable: {elf.HasSectionsStringTable}.");
-                                    Log.Info(Tag, $"HasSectionHeader: {elf.HasSectionHeader}.");
-                                    foreach (var section in elf.Sections)
-                                    {
-                                        Log.Info(Tag, $"Section: {section}.");
-                                    }
-                                    Log.Info(Tag, $"HasSegmentHeader: {elf.HasSegmentHeader}.");
-                                    foreach (var segment in elf.Segments)
-                                    {
-                                        Log.Info(Tag, $"Segment: {segment}.");
-                                    }
-                                    Log.Info(Tag, $"Endianess: {elf.Endianess}.");
-                                    Log.Info(Tag, $"Machine: {elf.Machine}.");
-                                    Log.Info(Tag, $"Type: {elf.Type}.");
-                                }
-                            }
-                            else
-                            {
-                                Log.Warn(Tag, $"Couldn't load': {file} with ELF reader.");
-                            }
-
-                        }
-                    }
-                    else
-                    {
-                        Log.Warn(Tag, $"Path {path} doesn't exist.");
-                    }
-                });
+                Parallel.ForEach(paths, Run);
             };
+        }
+
+        private void Run(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Log.Warn(Tag, $"Path {path} doesn't exist.");
+                return;
+            }
+
+            var files = Directory.GetFiles(path);
+            Log.Info(Tag, $"Path {path} has {files.Length} files to process");
+
+            foreach (var file in files)
+            {
+                Log.Info(Tag, $"File: {file}.");
+                if (!ELFReader.TryLoad(file, out var elf))
+                {
+                    Log.Warn(Tag, $"Couldn't load': {file} with ELF reader.");
+                    continue;
+                }
+
+                var hasBuildId = elf.TryGetSection(".note.gnu.build-id", out var buildId);
+                if (!hasBuildId)
+                {
+                    Log.Warn(Tag, $"No Debug Id in {file}");
+                    continue;
+                }
+
+                var hasUnwindingInfo = elf.TryGetSection(".eh_frame", out _);
+                var hasDwarfDebugInfo = elf.TryGetSection(".debug_frame", out _);
+
+                if (!hasUnwindingInfo && !hasDwarfDebugInfo)
+                {
+                    Log.Warn(Tag, $"No unwind nor DWARF debug info in {file}");
+                    continue;
+                }
+
+                ProcessFile(hasUnwindingInfo, hasDwarfDebugInfo, buildId, elf);
+            }
+        }
+
+        private static void ProcessFile(bool hasUnwindingInfo, bool hasDwarfDebugInfo, ISection buildId, IELF elf)
+        {
+            Log.Info(Tag, $"Contains unwinding info: {hasUnwindingInfo}");
+            Log.Info(Tag, $"Contains DWARF debug info: {hasDwarfDebugInfo}");
+
+            var builder = new StringBuilder();
+            var bytes = buildId.GetContents().Skip(16);
+
+            foreach (var @byte in bytes)
+            {
+                builder.Append((@byte).ToString("x2"));
+            }
+
+            Log.Info(Tag, $"Build Id: {builder}");
+
+            Log.Info(Tag, $"Class: {elf.Class}.");
+            Log.Info(Tag, $"HasSectionsStringTable: {elf.HasSectionsStringTable}.");
+            Log.Info(Tag, $"HasSectionHeader: {elf.HasSectionHeader}.");
+            foreach (var section in elf.Sections)
+            {
+                Log.Info(Tag, $"Section: {section}.");
+            }
+
+            Log.Info(Tag, $"HasSegmentHeader: {elf.HasSegmentHeader}.");
+            foreach (var segment in elf.Segments)
+            {
+                Log.Info(Tag, $"Segment: {segment}.");
+            }
+
+            Log.Info(Tag, $"Endianess: {elf.Endianess}.");
+            Log.Info(Tag, $"Machine: {elf.Machine}.");
+            Log.Info(Tag, $"Type: {elf.Type}.");
         }
     }
 }
