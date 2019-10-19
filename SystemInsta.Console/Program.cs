6eb0b82d3ed4c6bda5df33904c60c9c44bcb4063
Bruno Garcia <bruno@brunogarcia.com>
2019-10-18 22:38:01 -0400

    feat: console

---
+++ SystemInsta.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace SystemInsta.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Doing some work.");
+            await Task.Run(async () =>
+            {
+                var paths = new[] {"/sbin/"};
+                var uploader = new SystemImageUploader();
+
+                foreach (var path in paths)
+                {
+                    try
+                    {
+                        await uploader.Run(path);
+                    }
+                    catch (System.Exception e)
+                    {
+                        System.Console.WriteLine($"Failed: {e}");
+                    }
+                }
+            });
+        }
+    }
+
+    class Logger : ILogger
+    {
+
+    }
+}
